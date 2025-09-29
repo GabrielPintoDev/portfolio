50b5424980ad5a47c0cbb621c8f14cf8ff14b50e
GabrielPintoDev <gabrielandrade.colorado@gmail.com>
2025-09-29 20:52:37 -0300

    Construindo estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/GabrielPintoDev.png" />
+    <Titulo>Gabriel Andrade</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Title'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, ipsa est
+      deleniti aliquid consequuntur sed nulla repellat reprehenderit dolorem
+      vero nesciunt architecto, nemo rerum similique quis facilis magnam
+      corporis voluptatem?
+    </p>
+  </section>
+)
+
+export default Sobre
